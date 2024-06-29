67dd58369b63b30e7153ae3e3bb30074a416a593
camila <camilamaita07@gmail.com>
2024-06-29 04:19:28 -0300

    navigate added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Main } from "./components/Main";
 import { Home } from "./components/Home/Home";
 import { Us } from "./components/Us/Us";
 import { OurServices } from "./components/OurServices/OurServices";
@@ -11,18 +15,14 @@ function App() {
   return (
     <>
       <Router>
-        <Main>
-          <Navbar />
-
-          {/* resolver childrem en Main */}
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/Inicio" element={<Home />}></Route>
-            <Route path="/Nosotros" element={<Us />}></Route>
-            <Route path="/Servicios" element={<OurServices />}></Route>
-            <Route path="/Contacto" element={<Contact />}></Route>
-          </Routes>
-        </Main>
+        <Navbar />
+        <Routes>
+          <Route path="/*" element={<Navigate to="/Inicio" />}></Route>
+          <Route path="/Inicio" element={<Home />}></Route>
+          <Route path="/Nosotros" element={<Us />}></Route>
+          <Route path="/Servicios" element={<OurServices />}></Route>
+          <Route path="/Contacto" element={<Contact />}></Route>
+        </Routes>
       </Router>
     </>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
 import "./Main.css";
+
 export const Main = ({ children }: { children: ReactNode }) => {
   return <div>{children}</div>;
 };
+//pensar que utilidad le puedoi dar a este componente
